97fc921100b0a5466fb28b282003f0a1ff819916
Omar Abbood <omsa04@gmail.com>
2023-01-30 23:40:16 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header (){
+    return (
+        <div className="header">
+            <img className="headerImg" src="/Images/icon.png"></img>
+            <h3 className="headerTitle">my travel journal.</h3>
+        </div>
+    )
+}
